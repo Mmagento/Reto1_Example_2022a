bd87cbe1b0b3d159382972e08f2c424e3c91581e
ibaiarroyo <ibai.arroyodi@elorrieta-errekamari.com>
2022-11-08 19:26:47 +0100

    Cambio en el RegisterActivity

--- app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
+++ app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
@@ -4,28 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    public String nombre;
-    public String apellidos;
-    public String email;
-    public String password1;
-    public String password2;
+    public EditText nombre;
+    public EditText apellidos;
+    public EditText email;
+    public EditText password1;
+    public EditText password2;
     Button botonRegistro;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.activity_main );
+        setContentView(R.layout.activity_register);
 
-        botonRegistro.findViewById(R.id.botonRegistro);
+        Intent intentacanciones = new Intent(RegisterActivity.this, MainActivity.class);
 
+        // 1-Para transformar los datos en minusculas.
+        botonRegistro = findViewById(R.id.botonRegistro);
+        nombre = (EditText)findViewById(R.id.textNombre);
+        apellidos = (EditText)findViewById(R.id.textApellidos);
+        email = (EditText)findViewById(R.id.textEmail);
+        password1 = (EditText)findViewById(R.id.textPassword1);
+        password2 = (EditText)findViewById(R.id.textPassword2);
         botonRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
+                // 2-Para transformar los datos en minusculas.
+                nombre.toString().toLowerCase();
+                apellidos.toString().toLowerCase();
+                email.toString().toLowerCase();
+                password1.toString().toLowerCase();
+                password2.toString().toLowerCase();
+
+                //intentacanciones.putExtra();
+                RegisterActivity.this.startActivity(intentacanciones);
             }
         });
 
