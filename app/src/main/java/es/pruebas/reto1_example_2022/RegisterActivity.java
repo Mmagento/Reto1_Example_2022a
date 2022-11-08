9cb5cbac7154000c6799252e518e981282ad6c11
in1dm3-v <richardoswaldo.gavilanesfr@elorrieta-errekamari.com>
2022-11-08 16:57:58 +0100

    Añadido clase Register Activity

--- app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
+++ app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
@@ -1,11 +1,34 @@
 package es.pruebas.reto1_example_2022;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
     public String nombre;
     public String apellidos;
-    public String password;
+    public String email;
+    public String password1;
+    public String password2;
+    Button botonRegistro;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_main );
+
+        botonRegistro.findViewById(R.id.botonRegistro);
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+            }
+        });
+
+    }
 
 }
