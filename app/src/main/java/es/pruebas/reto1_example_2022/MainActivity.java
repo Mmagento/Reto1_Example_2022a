ef9ad7babc2b2bd5b75c49430ae3638bd67c09dc
in1dm3-v <richardoswaldo.gavilanesfr@elorrieta-errekamari.com>
2022-11-08 17:32:03 +0100

    Cambios en GestorConexiones

--- app/src/main/java/es/pruebas/reto1_example_2022/MainActivity.java
+++ app/src/main/java/es/pruebas/reto1_example_2022/MainActivity.java
@@ -24,69 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ArrayList<Video> listado = new ArrayList<>();
-
-        MyTableAdapter myTableAdapter = new MyTableAdapter (this, R.layout.myrow_layout, listado);
-        ((ListView) findViewById( R.id.listView)).setAdapter (myTableAdapter);
-
-        findViewById(R.id.getOneButton ).setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isConnected()) {
-                    VideoFacade videoFacade = new VideoFacade( 1 );
-                    Thread thread = new Thread( videoFacade );
-                    try {
-                        thread.start();
-                        thread.join(); // Awaiting response from the server...
-                    } catch (InterruptedException e) {
-                        // Nothing to do here...
-                    }
-
-                    // Processing the answer
-                    Video user = videoFacade.getResponse();
-                    listado.add( user );
-                    ((ListView) MainActivity.this.findViewById( R.id.listView )).setAdapter( myTableAdapter );
-                }
-            }
-        } );
-
-        findViewById(R.id.getAllButton ).setOnClickListener( v -> {
-            if (isConnected()) {
-                VideosFacade videosFacade = new VideosFacade();
-                Thread thread = new Thread( videosFacade );
-                try {
-                    thread.start();
-                    thread.join(); // Awaiting response from the server...
-                } catch (InterruptedException e) {
-                    // Nothing to do here...
-                }
-                // Processing the answer
-                ArrayList<Video> listVideos = videosFacade.getResponse();
-                listado.addAll( listVideos );
-                ((ListView) findViewById( R.id.listView)).setAdapter (myTableAdapter);
-            }
-        });
-    }
-
-    /**
-     * Returns true if we are conected to the network. False otherwise
-     *
-     * @return True or False
-     */
-    public boolean isConnected() {
-        boolean ret = false;
-        try {
-            ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext()
-                    .getSystemService( Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if ((networkInfo != null) && (networkInfo.isAvailable()) && (networkInfo.isConnected()))
-                ret = true;
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), getString(R.string.error_communication), Toast.LENGTH_SHORT).show();
-        }
-        return ret;
     }
 }
