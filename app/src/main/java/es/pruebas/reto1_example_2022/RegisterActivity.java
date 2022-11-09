2bdd881ea6b68e998d0d395fd932d634f5807566
ibaiarroyo <ibai.arroyodi@elorrieta-errekamari.com>
2022-11-09 16:14:35 +0100

    Cambios en el RegisterActivity para que los datos del usuario no sean nulos.

--- app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
+++ app/src/main/java/es/pruebas/reto1_example_2022/RegisterActivity.java
@@ -14,6 +14,8 @@ import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    public EditText Login;
+
     public EditText nombre;
     public EditText apellidos;
     public EditText email;
@@ -27,12 +29,16 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        botonRegistro = findViewById(R.id.botonRegistro);
+
+        Login = findViewById(R.id.textLogin);
+
         nombre = findViewById(R.id.textNombre);
         apellidos = findViewById(R.id.textApellidos);
         email = findViewById(R.id.textEmail);
         password1 = findViewById(R.id.textPassword1);
         password2 = findViewById(R.id.textPassword2);
+
+        botonRegistro = findViewById(R.id.botonRegistro);
 
         Intent intentacanciones = new Intent(RegisterActivity.this, MainActivity.class);
 
@@ -45,13 +51,18 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // 2-Para transformar los datos en minusculas.
 
+                String login = Login.getText().toString();
+
                 String mnombre = nombre.getText().toString().toLowerCase();
                 String mapellidos = apellidos.getText().toString().toLowerCase();
                 String memail = email.getText().toString().toLowerCase();
                 String mpassword1 = password1.getText().toString();
                 String mpassword2 = password2.getText().toString();
 
-                if(mpassword1.equals(mpassword2)){
+
+
+                if(mpassword1.equals(mpassword2) && !mnombre.isEmpty() && !mapellidos.isEmpty() && !memail.isEmpty() && !mpassword1.isEmpty() && !mpassword2.isEmpty()){
+
                     usuario.setApellidos(mapellidos);
                     usuario.setEmail(memail);
                     usuario.setNombre(mnombre);
@@ -59,13 +70,15 @@ public class RegisterActivity extends AppCompatActivity {
 
                     dataManager.insert(usuario);
                     Toast.makeText(getApplicationContext(), getString( R.string.insertadocorrectamente ), Toast.LENGTH_LONG).show();
+                    startActivity(intentacanciones);
 
                 }else{
                     Toast.makeText(getApplicationContext(), getString( R.string.insertadonocorrectamente ), Toast.LENGTH_LONG).show();
 
                 }
 
-                RegisterActivity.this.startActivity(intentacanciones);
+                //intentacanciones.putExtra("Login",login);
+
 
 
             }
